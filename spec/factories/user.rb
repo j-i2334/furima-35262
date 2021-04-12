63ae9648bc4367af16652a26fd2c06b7cf4b2027
j-i2334 <80821404+j-i2334@users.noreply.github.com>
2021-04-12 19:11:57 +0900

    モデルの単体テストコード　修正7

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     last_name_kana        { 'ヤマダ' }
     first_name_kana       { 'タロウ' }
     birthday { '2010-01-01' }
-    password = Faker::Internet.password(min_length: 6)
+    password = '1a' + Faker::Internet.password(min_length: 6)
     password { password }
     password_confirmation { password }
   end
