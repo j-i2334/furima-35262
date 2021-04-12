e4b405858842fdc7e5758bfaa94244e1524f97a8
j-i2334 <80821404+j-i2334@users.noreply.github.com>
2021-04-12 17:16:12 +0900

    モデルの単体テストコード　修正4

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
   with_options presence: true do
-    validates :first_name
-    validates :last_name
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/ } do
-      validates :first_name
-      validates :last_name
-    end
+      validates :nickname
+      validates :birthday
   end
 
-  with_options presence: true do
-    validates :first_name_kana
-    validates :last_name_kana
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+  validates :password, :password_confirmation, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}\z/ }
+
+  with_options presence: true, format: {  with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+      validates :first_name
+      validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
       validates :first_name_kana
       validates :last_name_kana
-    end
   end
-  validates :birthday, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,12 @@ RSpec.describe User, type: :model do
       end
     end
     context '新規登録できないこと' do
+      it 'nicknameが空では登録できないこと' do
+        @user.nickname = ' '
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
       it 'emailが空では登録できないこと' do
         @user.email = ' '
         @user.valid?
