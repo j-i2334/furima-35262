f664c29871a12b2812a98f45bb92f17ce9db004a
j-i2334 <80821404+j-i2334@users.noreply.github.com>
2021-04-15 08:39:42 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :shipping_charges
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :product_condition_id, numericality: { other_than: 1 }
+  validates :shipping_charges_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 0 }
+  validates :days_to_ship_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
+  validates :image, presence: true
+
   belongs_to :user
-  has_one :purchase_record
+  # has_one :purchase_record
+  has_one_attached :image
 end
