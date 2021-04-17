b49e330823bef9cb3fe83af4baba054258febfe1
j-i2334 <80821404+j-i2334@users.noreply.github.com>
2021-04-17 12:03:27 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :price, :product_condition_id, :shipping_charges_id, :shipping_area_id, :days_to_ship_id, :category_id, :image).merge(user_id: current_user.id) 
