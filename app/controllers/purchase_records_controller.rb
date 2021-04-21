38d016d245af1133beee8476ab68e99de046aece
j-i2334 <80821404+j-i2334@users.noreply.github.com>
2021-04-21 15:15:32 +0900

    商品購入機能の実装

---
+++ app/controllers/purchase_records_controller.rb
@@ -0,0 +1,44 @@
+class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create, :sold_out_item]
+  before_action :sold_out_item, only: [:index]
+
+  def index
+    @purchase_record_destination = PurchaseRecordDestination.new
+  end
+  
+  def create
+    @purchase_record_destination = PurchaseRecordDestination.new(purchase_record_params)
+    if @purchase_record_destination.valid?
+      pay_item
+      @purchase_record_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_record_params
+    params.require(:purchase_record_destination).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: purchase_record_params[:token],
+      currency: 'jpy' 
+    )
+  end
+
+  def sold_out_item
+    redirect_to root_path if @item.purchase_record.present?
+   end
+
+   def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -27,6 +27,6 @@ class Item < ApplicationRecord
     }
 
   belongs_to :user
-  # has_one :purchase_record
+  has_one :purchase_record
   has_one_attached :image
 end
